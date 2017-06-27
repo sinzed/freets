02da41410012c19a2dbec939190972e9e5a4d536
Saeed <saeed1@fb3.uni-bremen.de>
2017-06-27 23:53:24 +0200

    backend updated

--- api/DatabaseHandler.php
+++ api/DatabaseHandler.php
@@ -21,7 +21,7 @@ class DatabaseHandler extends Connection {
 //		$password = $conn->real_escape_string($password);
 
 		
-		$query="SELECT * From users";
+		$query="SELECT * From users ";
 		$result = $this->connection->query($query);
 		$outp = "";
 		
@@ -29,12 +29,87 @@ class DatabaseHandler extends Connection {
 			if ($outp != "") {$outp .= ",";}
 			$outp .= '{"u_id":"'  . $rs["id"] . '",';
 			$outp .= '"u_email":"'   . $rs["email"]        . '",';
+			$outp .= '"u_name":"'   . $rs["name"]        . '",';
 			$outp .= '"u_password":"'. $rs["password"]     . '"}';
 		}
 		$outp ='{"records":['.$outp.']}';
 		$this->connection->close();
 
 		echo($outp);
+    }
+    function getUser($data){
+        // To protect MySQL injection for Security purpose
+		$email = stripslashes($data['email']);
+		$password = stripslashes($data['password']);
+//		$username = $conn->real_escape_string($username);
+//		$password = $conn->real_escape_string($password);
+
+		
+		$query="SELECT name, email, phone, address FROM users  
+				where email like '".$email."' and password like '".$password."'";
+		$result = $this->connection->query($query);
+		$outp = "";
+		if( $rs= mysqli_fetch_assoc($result)) {
+			if ($outp != "") {$outp .= ",";}
+			$outp .= '{"u_name":"'  . $rs["name"] . '",';
+			$outp .= '"is_user":"true",';
+			$outp .= '"u_phone":"'   . $rs["phone"]        . '",';
+			$outp .= '"u_address":"'. $rs["address"]     . '"}';
+                 
+		}
+                else {
+                        $outp = '{"is_user":"false"}';
+                }
+                $outp ='{"records":'.$outp.'}';
+
+		$this->connection->close();
+
+		return $outp;
+        
+    }
+    function isRegistered($data){
+        $email  = $data['email'];
+        
+        $query="SELECT * FROM users where email = '$email' ";
+       
+        $result = $this->connection->query($query) or die ("we can not check user");
+        
+        $data = mysqli_fetch_array($result, MYSQLI_NUM);
+		
+        if($data[0] > 1) {
+                $outp='{"result":{"created": "0" , "exists": "1" }}';
+        }
+        else
+            return false;
+        
+        return $outp;
+        
+    }
+    function createUser($data){
+        if($result = $this->isRegistered($data))
+                return $result;
+        $name  = $data['name'];
+        $email  = $data['email'];
+        $password  = $data['password'];
+        $phone  = $data['phone'];
+        $address  = $data['address'];
+        $query="INSERT into users (`email`,`password`,`phone`,`address` ,`name`) VALUES ('" 
+					. $email."' ,'"
+					. $password."' ,'"
+					. $phone."' ,'"
+					. $address."' ,'"
+					. $name."'"
+					.");";
+        $result = $this->connection->query($query) or die ("error we cant create  user");
+        
+        if ($outp != "") {$outp .= ",";}
+                $outp .= '{"created":"1",';
+                $outp .= '"exists":"0"}';
+        $outp ='{"result":'.$outp.'}';
+        $this->connection->close();
+
+        return $outp;
+
     }
 }
 ?>
--- api/users.php
+++ api/users.php
@@ -1,4 +1,7 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
 include "DatabaseHandler.php";
 $db = new DatabaseHandler();
 $db->getUsers();
